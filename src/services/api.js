cc4d401fb6ca87928c84535e248beb2b2fe8fbde
Naveen Kumar <prineee@gmail.com>
2026-05-17 01:19:45 +0530

    Connected frontend to backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import API_URL from "../config/api";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: API_URL,
 });
 
 export default api;
