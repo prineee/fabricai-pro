4a778778a1f3f4969dadbd7482b7390343cd6940
Naveen Kumar <prineee@gmail.com>
2026-05-22 23:49:00 +0530

    Fixed App.tsx imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import Pricing from "./pages/Pricing";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/signup"
+          element={<Signup />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
+        <Route
+          path="/pricing"
+          element={<Pricing />}
+        />
       </Routes>
     </BrowserRouter>
   );
