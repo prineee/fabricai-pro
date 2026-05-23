cf2ec8f3a00e70ceefafc67deff2d83dd2fab751
Naveen Kumar <prineee@gmail.com>
2026-05-23 13:54:20 +0530

    Created stable app version

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,10 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-import Overview from "./pages/dashboard/Overview";
-import BlogGenerator from "./pages/dashboard/BlogGenerator";
-import AdGenerator from "./pages/dashboard/AdGenerator";
-import EmailGenerator from "./pages/dashboard/EmailGenerator";
-import History from "./pages/dashboard/History";
-import Billing from "./pages/dashboard/Billing";
-import Settings from "./pages/dashboard/Settings";
-
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import AffiliateDashboard from "./pages/affiliate/AffiliateDashboard";
-
-import ProtectedRoute from "./components/ProtectedRoute";
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* PUBLIC */}
 
         <Route
           path="/"
@@ -41,93 +26,6 @@ export default function App() {
         <Route
           path="/register"
           element={<Register />}
-        />
-
-        {/* DASHBOARD */}
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Overview />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/dashboard/blog"
-          element={
-            <ProtectedRoute>
-              <BlogGenerator />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/dashboard/ad"
-          element={
-            <ProtectedRoute>
-              <AdGenerator />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/dashboard/email"
-          element={
-            <ProtectedRoute>
-              <EmailGenerator />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/dashboard/history"
-          element={
-            <ProtectedRoute>
-              <History />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/dashboard/billing"
-          element={
-            <ProtectedRoute>
-              <Billing />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/dashboard/settings"
-          element={
-            <ProtectedRoute>
-              <Settings />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* ADMIN */}
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* AFFILIATE */}
-
-        <Route
-          path="/affiliate"
-          element={
-            <ProtectedRoute>
-              <AffiliateDashboard />
-            </ProtectedRoute>
-          }
         />
 
       </Routes>
